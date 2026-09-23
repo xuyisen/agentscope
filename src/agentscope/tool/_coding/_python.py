b302572e553ecff1ca57a3a260ea620331fd1b4d
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-22 21:25:51 -0400

    xuyisen__diff__id_345

--- src/agentscope/tool/_coding/_python.py
+++ src/agentscope/tool/_coding/_python.py
@@ -12,8 +12,10 @@ import shortuuid
 
 from ...message import TextBlock
 from .._response import ToolResponse
+
 os.environ["PYTHONUTF8"] = "1"
 os.environ["PYTHONIOENCODING"] = "utf-8"
+
 
 async def execute_python_code(
     code: str,
